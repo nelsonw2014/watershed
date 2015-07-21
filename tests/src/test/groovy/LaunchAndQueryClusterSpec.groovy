43417a29ceb7bbd5e71a4df65bc3332ab28550cf
Paul Mogren <pmogren@commercehub.com>
2015-07-21 13:45:10 -0400

    Added upload-resources to the test, found a bug. The system works!
    Former-commit-id: e244f93a053c019bc209cf6626c6a7ed6a3c7b24

--- tests/src/test/groovy/LaunchAndQueryClusterSpec.groovy
+++ tests/src/test/groovy/LaunchAndQueryClusterSpec.groovy
@@ -16,12 +16,6 @@ class LaunchAndQueryClusterSpec extends Specification {
     String clusterId
 
     def "launch cluster and issue queries"() {
-        //TODO upload-resources or assert it has been done
-        //TODO stream and archives need data, can that be generated now?
-        //TODO generate config file that won't conflict with anything in actual use? Meh, we need to be able to test a real config; can create a dummy one as a special case or in a harness.
-        //TODO in order to run query, need SSH proxy
-        //TODO Doesn't this really belong in OSS? Let user configure queries?
-
         given: "Test configuration"
         String launcherHome = System.getProperty("launcherHome")
         if (!launcherHome || !new File(launcherHome).isDirectory()) {
@@ -38,9 +32,16 @@ class LaunchAndQueryClusterSpec extends Specification {
             throw new IllegalArgumentException("You must set the system property privateKeyFile to the local file containing the private key for the SSH key pair used by the EC2 nodes in the cluster.")
         }
 
+        when: "Uploading resources"
+        Integer exitCode = new Shell().execute(System.out, 1, TimeUnit.MINUTES,
+            "${launcherHome}/upload-resources", "${launcherConfig}")
+
+        then: "Resources uploaded"
+        exitCode == 0
+
         when: "Launching a cluster"
         def capturedOutputStream = new ByteArrayOutputStream()
-        Integer exitCode = new Shell().execute(new PrintStream(capturedOutputStream), 1, TimeUnit.MINUTES,
+        exitCode = new Shell().execute(new PrintStream(capturedOutputStream), 1, TimeUnit.MINUTES,
                 "${launcherHome}/launch-cluster", "${launcherConfig}")
         String capturedOutput = capturedOutputStream.toString()
         System.out.print(capturedOutput)
@@ -102,28 +103,6 @@ class LaunchAndQueryClusterSpec extends Specification {
         then: "Query result is correct"
         rs.size() == 1
         rs[0].getProperty("cnt") == 1155
-
-/*
-        when:
-        //TODO configure a stream archive storage plugin?
-        //TODO issue stream archive query
-
-        then:
-        //TODO assert stream archive query success
-
-        when:
-        //TODO configure a stream storage plugin?
-        //TODO issue stream query
-
-        then:
-        //TODO assert stream query success
-
-        when:
-        //TODO issue unified-view query
-
-        then:
-        //TODO assert unified-view query success
-*/
 
         cleanup:
         logger.info("Cleaning up.")

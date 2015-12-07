ee9a6783a16a7bcbb23b205599ec89c5e61eedaa
Curt Hostetter <chostetter@gavant.com>
2015-12-07 13:34:24 -0500

    add javadoc to the new classes

--- pump/src/main/java/com/commercehub/watershed/pump/model/PumpRecord.java
+++ pump/src/main/java/com/commercehub/watershed/pump/model/PumpRecord.java
@@ -2,6 +2,9 @@ package com.commercehub.watershed.pump.model;
 
 import com.amazonaws.services.kinesis.model.Record;
 
+/**
+ * Holds both a KinesisRecord and the ResultRow retrieved from Drill.
+ */
 public class PumpRecord {
     Record kinesisRecord;
     ResultRow drillRow;
@@ -11,10 +14,18 @@ public class PumpRecord {
         this.drillRow = drillRow;
     }
 
+    /**
+     * Retrieve the Kinesis record
+     * @return Record
+     */
     public Record getKinesisRecord() {
         return kinesisRecord;
     }
 
+    /**
+     * Retrieve the Drill row
+     * @return ResultRow
+     */
     public ResultRow getDrillRow() {
         return drillRow;
     }
--- pump/src/main/java/com/commercehub/watershed/pump/model/PumpRecordResult.java
+++ pump/src/main/java/com/commercehub/watershed/pump/model/PumpRecordResult.java
@@ -2,6 +2,9 @@ package com.commercehub.watershed.pump.model;
 
 import com.amazonaws.services.kinesis.producer.UserRecordResult;
 
+/**
+ * Holds results for the KinesisRecord and the row that was pulled from Drill.
+ */
 public class PumpRecordResult {
     private UserRecordResult userRecordResult;
     private ResultRow resultRow;
@@ -11,10 +14,18 @@ public class PumpRecordResult {
         this.resultRow = resultRow;
     }
 
+    /**
+     * Get the Kinesis UserRecordResult
+     * @return UserRecordResult
+     */
     public UserRecordResult getUserRecordResult() {
         return userRecordResult;
     }
 
+    /**
+     * Get the row that was pulled from Drill.
+     * @return
+     */
     public ResultRow getResultRow() {
         return resultRow;
     }

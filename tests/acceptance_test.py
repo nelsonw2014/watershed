86d9c36712aa515b47deec61c070e41ab6df234a
Warren Nelson <nelsonw2014@gmail.com>
2016-02-02 16:34:01 -0500

    Acceptance test now queries against sample data and leverages both query.json as well as PumpClient

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -4,13 +4,19 @@ import requests
 import json
 from signal import SIGINT
 from time import sleep
-from nose.tools import assert_equal, assert_in
+from pump_client.PumpClient import PumpClient
+from nose.tools import assert_equal, assert_in, assert_is_not_none
 
 def end_to_end_test():
     """Launch and query cluster"""
 
+    stream = os.environ['KINESIS_STREAM']
+
     conf_file = os.environ['WATERSHED_CONFIG']
     assert os.path.isfile(conf_file)
+
+    with open(conf_file, "r") as f:
+        conf = json.load(f)
 
     private_key_file = os.environ['PRIVATE_KEY_FILE']
     assert os.path.isfile(private_key_file)
@@ -64,7 +70,7 @@ def end_to_end_test():
         sleep(1)
 
         r = None
-        for retry in range(0, 60):
+        for retry in range(0, 10):
             try:
                 r = requests.post(
                     'http://localhost:8047/query.json',
@@ -84,10 +90,89 @@ def end_to_end_test():
                 query = json.loads(r.text)
                 assert_equal(len(query["rows"]), 1)
                 assert_in("cnt", query["rows"][0])
-                assert_equal(query["rows"][0]["cnt"] == "1155")
+                assert_equal(query["rows"][0]["cnt"], "1155")
                 break
+            if retry == 10 and r is None:
+                raise ConnectionRefusedError("Unable to connect to Drill.")
+            sleep(1)
+        r = None
+        for retry in range(0, 10):
+            try:
+                r = requests.post(
+                    'http://localhost:8047/query.json',
+                    json.dumps({
+                        "queryType": "SQL",
+                        "query": "SELECT * FROM `{}`.`sample_data`.* LIMIT 1".format(conf["AWS"]["S3"]["resourcesBucket"])
+                    }),
+                    headers={
+                        "Content-Type": "application/json"
+                    }
+                )
+            except Exception:
+                pass
+
+            if r is not None and hasattr(r, "status_code"):
+                assert_equal(r.status_code, 200)
+                query = json.loads(r.text)
+                assert_in("eventId", query["columns"])
+                assert_in("seller", query["columns"])
+                assert_in("buyer", query["columns"])
+                assert_in("price", query["columns"])
+                assert_in("productId", query["columns"])
+                assert_in("kinesisStreamName", query["columns"])
+                assert_in("timeCreated", query["columns"])
+                assert_in("valid", query["columns"])
+                assert_in("partitionKey", query["columns"])
+                assert_in("rawData", query["columns"])
+                assert_equal("true", query["rows"][0]["valid"])
+                assert_equal(query["rows"][0]["eventId"], query["rows"][0]["partitionKey"])
+                break
+            if retry == 10 and r is None:
+                raise ConnectionRefusedError("Unable to connect to Drill.")
+            sleep(1)
+        r = None
+        for retry in range(0, 10):
+            try:
+                r = requests.post(
+                    'http://localhost:8047/query.json',
+                    json.dumps({
+                        "queryType": "SQL",
+                        "query": "SELECT COUNT(*) cnt FROM `{}`.`sample_data`.* WHERE price>80".format(conf["AWS"]["S3"]["resourcesBucket"])
+                    }),
+                    headers={
+                        "Content-Type": "application/json"
+                    }
+                )
+            except Exception:
+                pass
+
+            if r is not None and hasattr(r, "status_code"):
+                assert_equal(r.status_code, 200)
+                query = json.loads(r.text)
+                assert_equal(len(query["rows"]), 1)
+                assert_in("cnt", query["rows"][0])
+                assert_equal(query["rows"][0]["cnt"], "257")
+                break
+            if retry == 10 and r is None:
+                raise ConnectionRefusedError("Unable to connect to Drill.")
             sleep(1)
 
+        client = PumpClient("http://localhost:8080/pump")
+
+        job = client.preview_job("SELECT * FROM `{}`.`sample_data`.* WHERE price>80".format(conf["AWS"]["S3"]["resourcesBucket"]), 1)
+        assert_equal(job["count"], 257)
+        assert_in("partitionKey", job["rows"][0])
+        assert_in("rawData", job["rows"][0])
+
+        job = client.create_job("SELECT * FROM `{}`.`sample_data`.* WHERE price>80".format(conf["AWS"]["S3"]["resourcesBucket"]), stream, False, True, True)
+        assert_is_not_none(job["jobId"])
+        job_id = job["jobId"]
+        assert_equal(job["stage"], "NOT_STARTED")
+
+        job = client.get_job(job_id, True, False)
+        assert_equal(job["stage"], "COMPLETED_SUCCESS")
+        assert_equal(job["successfulRecordCount"], 257)
+        assert_equal(len(job["processingErrors"]), 0)
     finally:
         if forwarding_process is not None:
             forwarding_process.send_signal(SIGINT)
